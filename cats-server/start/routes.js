bf8daa73fb97b94d9a412447142cdbe1ddd3dce7
felipesergios <felipe.sergio2011@gmail.com>
2020-06-08 11:41:03 -0300

    Add Validator

--- cats-server/start/routes.js
+++ cats-server/start/routes.js
@@ -20,12 +20,13 @@ Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
 
 
-
+Route.resource('gatos','CatController').apiOnly()
+Route.get('files/:file','FileController.show')
 
 Route.group(() => {
     Route.post('file','FileController.store')
-    Route.get('files/:file','FileController.show')
+    
     Route.get('files','FileController.index')
-    Route.resource('gatos','CatController').apiOnly()
+    
     Route.get('status','CatController.status')
   }).middleware(['auth'])
